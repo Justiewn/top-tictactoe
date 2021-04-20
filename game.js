8802ac19eb1501bee967deb32ea21fb4eb6dbd0b
Justin W <justinwei.dev@gmail.com>
2021-04-20 15:09:52 +1200

    added turns and move and win conditions

--- game.js
+++ game.js
@@ -2,29 +2,103 @@
 
 
 const Gameboard = () => {
-    let gameArray = ['✕', '✕', '◯', '✕', '✕', '◯', '◯', '◯', '✕'];
+    let gameArray = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
 
-    const addToArray = (xo, sq) => {
-        gameArray[sq] = xo;
+    const addToArray = (xo, xIndex, yIndex) => {
+        gameArray[xIndex][yIndex] = xo;
     }
-    return {gameArray, addToArray};
+    const allowMove = (xIndex, yIndex) => {
+        if (gameArray[xIndex][yIndex] == ' ') return true;
+        return false;
+    }
+    const getArray = () => {
+        return gameArray;
+    }
+
+    const resetGame = () => {
+        gameArray = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    }
+    return {getArray, addToArray, allowMove, resetGame};
 };
 
 const Logic = () => {
-    const playerList = []
+    const playerList = [];
+    let playerTurn = false;           // false for first player, true for second
+    let thisGame;
+
+    const setGame = (game) => {
+        thisGame = game;
+    }
+
     const addPlayer = (player) => {
         if (playerList.length < 2) {
             addPlayer.push(player);
         } else console.log("max players reached");
     }
-    
-    const updateBoard = (list) => {
-        for (let i = 0; i < 9; i++) {
-            const gridToAdd = document.querySelector(`#sq-${i}`);
-            gridToAdd.innerHTML = list[i];
+    const playerMove = (xIndex, yIndex) => {
+        thisGame.addToArray(useXO(), xIndex, yIndex);
+    }
+    const updateBoard = () => {
+        list = thisGame.getArray()
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                const gridToAdd = document.querySelector(`#sq-${x}-${y}`);
+                gridToAdd.innerHTML = list[x][y];
+            }
         }
     }
-    return {updateBoard};
+    const checkWin = () => {
+        const arr =thisGame.getArray();
+        if (checkRows(arr) || checkCols(arr) || checkDiag(arr) ) {
+            console.log(`${useXO()} wins!`)
+            return true;
+        }
+        playerTurn = !playerTurn;
+    }
+
+    const checkRows = (array) => {
+        for (let i = 0; i < 3; i++) {          
+            const row = array[i];
+            if (row.every(val => val == useXO())) {
+                return true
+            }
+        }
+    }
+    const checkCols = (array) => {
+        for (let i = 0; i < 3; i++) {             
+            let column = [];
+            for (let j = 0; j < 3; j++) {           
+                 column.push(array[j][i]);
+            }
+            if (column.every(val => val == useXO())) {
+                return true
+            }
+        }
+    }
+    const checkDiag = (array) => {
+        let diag1 = [];
+        let diag2 = [array[0][2], array[1][1], array[2][0]];
+        for (let i = 0; i < 3; i++) {             
+            for (let j = 0; j < 3; j++) {   
+                if (i == j) diag1.push(array[j][i]);
+            }
+        }
+        console.log(diag1);
+        if (diag1.every(val => val == useXO()) || diag2.every(val => val == useXO())) {
+            return true
+        }
+    }
+    
+    const reset = () => {
+        playerTurn = false;
+        thisGame.resetGame();
+        updateBoard();
+
+    }
+    const useXO = () => {
+        return (!playerTurn) ? '✕':'◯'
+    }
+    return {setGame, updateBoard, playerMove, checkWin, reset};
 };
 
 const Player = (name) => {
@@ -36,13 +110,19 @@ const master = Logic();
 const grids = document.querySelectorAll('.game-grid');
 
 
-master.updateBoard(game.gameArray);
+master.setGame(game);
 
 grids.forEach(grid => {
     grid.addEventListener("click", (e) => {
-        console.log('yes');
-        indexToAdd = e.target.getAttribute("data-no");
-        game.addToArray("GA", indexToAdd);
-        master.updateBoard(game.gameArray);
+        
+        let xIndex, yIndex;
+        [xIndex, yIndex] = e.target.getAttribute("data-no").split('-');
+        if (game.allowMove(xIndex, yIndex)) {
+            master.playerMove(xIndex, yIndex);
+            master.updateBoard();
+            if (master.checkWin()) {
+                master.reset();
+            }
+        }
     })
 })
